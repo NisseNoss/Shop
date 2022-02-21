6fac655af5cb2ad45477c65f8f03259c4f9748c7
NonStop-png <e-elivaa1@asvg.no>
2022-02-21 21:10:34 +0100

    Styling to Cards

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="/CSS/style.css"
+    <link rel="stylesheet" href="/CSS/style.css"x
 </head>
 <body>
-
+    <div class="card">
+        <img src="">
+        <div class="card-interface">
+            <div class="prod-info">
+                <h3 class="headercolor">Fisk</h3>
+                <p class="color">89.99 kr</p>
+                <p class="color">5 på lager</p>
+            </div>
+            <button class="buy-button"></button>
+        </div>
+    </div>
 </body>
 </html>

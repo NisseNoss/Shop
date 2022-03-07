66cc5fd68ded2bebf1220a0d3c33cf334b65a002
NonStop-png <e-elivaa1@asvg.no>
2022-03-07 18:16:07 +0100

    Header

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
     <title>Hello world</title>
 </head>
 <body>
+<header>
+    <div id="header">
+        <a href="">
+            <img id="logo" src="" alt="Min logo">
+        </a>
+        <div id="menu">
+            <a class="menu-btn" href="#">Knapp</a>
+            <a class="menu-btn" href="#">Knapp</a>
+            <a class="menu-btn" href="#">Knapp</a>
+        </div>
+    </div>
+</header>
 <section id="cardsec">
     <?php
         include "products/loadcard.php";
